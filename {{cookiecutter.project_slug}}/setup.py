846f999009e050820c10fb9efdf673a8273b845a
Tom <tom@realfactors.com>
2020-06-19 17:13:05 -0400

    fix click cli boilerplate

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -45,7 +45,7 @@ setup(
     {%- if 'no' not in cookiecutter.command_line_interface|lower %}
     entry_points={
         'console_scripts': [
-            '{{ cookiecutter.project_slug }}={{ cookiecutter.project_slug }}.cli:main',
+            '{{ cookiecutter.project_slug }}={{ cookiecutter.project_slug }}.cli:cli',
         ],
     },
     {%- endif %}
--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/cli.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/cli.py
@@ -27,12 +27,24 @@ logger = logging.getLogger()
 {%- endif %}
 
 {% if cookiecutter.command_line_interface|lower == 'click' %}
-@click.command()
-def main(args=None):
-    """Console script for {{cookiecutter.project_slug}}."""
+class Config:
+    """Configuration Object"""
+CONTEXT = click.make_pass_decorator(Config, ensure=True)
+
+@click.group()
+@CONTEXT
+def cli(ctx):
+    '''
+        Console script for {{cookiecutter.project_slug}}.
+    '''
     click.echo("Replace this message by putting your code into "
-               "{{cookiecutter.project_slug}}.cli.main")
+               "{{cookiecutter.project_slug}}.cli.cli")
     click.echo("See click documentation at https://click.palletsprojects.com/")
+    return 0
+
+@cli.command('command')
+@CONTEXT
+def remove_dupe(ctx):
     return 0
 {%- endif %}
 {%- if cookiecutter.command_line_interface|lower == 'argparse' %}
